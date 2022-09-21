b33c457bde2009316c1c304227f48b26f51e9b84
Simon Mayer <simon.mayer90@gmail.com>
2022-09-21 14:20:34 +0200

    asdf

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.markdown("<h1 style='text-align: center;'>🔍 PCB Inspector 🔍</h1>", unsa
 
 image = Image.open('https://github.com/simonmayer90/python-deep-learning-image-recognition/blob/main/pcb-image.jpg')
 
-st.image(image)
+# st.image(image)
 
 # %% APP WORKFLOW
 st.markdown("""
