bde284d707c5f4de55927c2935aafd77192de9ec
Simon Mayer <simon.mayer90@gmail.com>
2022-09-22 10:12:07 +0200

    asdf

--- app.py
+++ app.py
@@ -65,30 +65,27 @@ if inspectButton == 1:
     converted_template_img = np.array(template_img.convert('RGB'))
     converted_test_img = np.array(test_img.convert('RGB'))
 
-    # gray1 = cv2.cvtColor(converted_template_img, cv2.COLOR_BGR2GRAY)        #converting to gray scale
-    # gray2 = cv2.cvtColor(converted_test_img, cv2.COLOR_BGR2GRAY)
-    # ret,thresh1 = cv2.threshold(gray1,200,255,cv2.THRESH_BINARY)  #applying threshold (binary) 
-    # ret,thresh2 = cv2.threshold(gray2,200,255,cv2.THRESH_BINARY)
-    # res =cv2.bitwise_xor(thresh1, thresh2, mask=None)      #comparing the images     
-    # st.image(res)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
     cv2.imwrite("template.jpg", converted_template_img)
     cv2.imwrite("test.jpg", converted_test_img)
 
-    rgb_template_img = cv2.imread('template.jpg')  
+    rgb_template_img = cv2.imread('template.jpg')
+    rgb_test_img = cv2.imread('test.jpg')  
 
-    # read template PCB 01 image as grayscale image
-    template_img = cv2.imread('template.jpg', 0)  
+    # read images as grayscale
+    template_img = cv2.imread('template.jpg', 0)
+    test_img = cv2.imread('test.jpg', 0)
     # the 2nd parameter is flag, makes image grayscale for value 0 or 2
 
-    # resize template image of PCB
-    newimgheight = template_img.shape[0]*(750/template_img.shape[1])
-    template_img_resize = cv2.resize(template_img, (750, int(newimgheight)))
+    # resize images
+    width = test_img.shape[1]
+    height = test_img.shape[0]
+
+    template_img_resize = cv2.resize(template_img, (width, height))
+    test_img_resize = test_img
 
     # Gaussian blur to blur the image before thresholding
     blur_template_img = cv2.GaussianBlur(template_img_resize, (3,3),0)
+    blur_test_img = cv2.GaussianBlur(test_img_resize, (3,3),0)
 
 
     # Adaptive thresholding(mean)
@@ -98,33 +95,15 @@ if inspectButton == 1:
                                             cv2. ADAPTIVE_THRESH_MEAN_C,
                                             cv2.THRESH_BINARY, 15, 5)
 
-
-    # read test image of PCB 01
-    rgb_test_img = cv2.imread('test.jpg')  
-
-    # read grayscale test PCB image
-    test_img = cv2.imread('test.jpg', 0)
-
-    # resize test image of PCB
-    width = template_img_resize.shape[1]
-    height = template_img_resize.shape[0]
-    test_img_resize = cv2.resize(test_img, (width, height))
-
-    # Gaussian blur to blur the image before thresholding
-    blur_test_img = cv2.GaussianBlur(test_img_resize, (3,3),0)
-
-    # Adaptive thresholding(mean) on test image
     test_adap_thresh = cv2.adaptiveThreshold(blur_test_img, 255, 
                                             cv2. ADAPTIVE_THRESH_MEAN_C,
                                             cv2.THRESH_BINARY, 15, 5)
 
     # Image subtraction (template - test)
-
     sub_img= cv2.subtract(template_adap_thresh, test_adap_thresh)
 
     # Median blur to eliminate background noise
     final_img = cv2.medianBlur(sub_img, 5)
-
 
     st.markdown("""
     ## Template PCB:
